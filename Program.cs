38b2e870db347776d6a756f410a7763fb998ac1d
MarkMolabola <mark.molabola@outlook.com>
2025-07-15 09:36:41 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Challenge_Labs_2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string realUserName = "admin";
+            string realPassword = "password";
+            int attempts = 0;
+            bool continueProgram = true;
+
+            do
+            {
+                Console.WriteLine("Enter UserID: ");
+                string userId = Console.ReadLine();
+                Console.WriteLine("Enter Password: ");
+                string password = Console.ReadLine();
+
+                if (userId == "admin")
+                {
+                    Console.WriteLine("Congrats you got it!");
+                    continueProgram = false;
+                }
+                else
+                {
+
+                    attempts++;
+                    Console.WriteLine("Incorrect UserID. Please try again.");
+
+                    if (attempts == 3)
+                    {
+                        Console.WriteLine("Max attempts used...exiting Program..");
+                        continueProgram = false;
+
+                    }
+
+                }
+            } while (continueProgram);
+            return;
+
+
+
+
+
+
+
+        }
+    }
+}
